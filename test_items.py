7cceb664f6a8e813dc741a33368b5f9f5a8b061f
Ryazolga <pleshkva.olga@gmail.com>
2019-07-04 17:04:45 +0500

    Add time.sleep in test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
 def test_add_to_cart_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
+    link1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link1)
     time.sleep(30)
     assert browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket"), "Button not found"
